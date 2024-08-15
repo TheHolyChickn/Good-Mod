a562f7f6f0ffaf726b83b6f38edb716f679b169a
GitHub Action <action@github.com>
2024-08-15 07:23:16 +0000

    Initialize template repository proper
    Set package name to com.github.theholychickn.theholychicknaddons
    Set project name to TheHolyChicknAddons
    Set modid to theholychicknaddons

--- src/main/java/com/github/theholychickn/theholychicknaddons/ExampleMod.java
+++ src/main/java/com/github/theholychickn/theholychicknaddons/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.theholychickn.theholychicknaddons;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "theholychicknaddons", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/theholychickn/theholychicknaddons/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/theholychickn/theholychicknaddons/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.theholychickn.theholychicknaddons.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
