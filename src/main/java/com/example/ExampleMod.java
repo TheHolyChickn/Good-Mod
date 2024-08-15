43a69a4ed20c96634e7b87b763a0bd21fc5dd878
The Holy Chicken <167555771+TheHolyChickn@users.noreply.github.com>
2024-08-15 03:23:01 -0400

    Initial commit

---
+++ src/main/java/com/example/ExampleMod.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "examplemod", useMetadata=true)
+public class ExampleMod {
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println("Dirt: " + Blocks.dirt.getUnlocalizedName());
+		// Below is a demonstration of an access-transformed class access.
+        System.out.println("Color State: " + new GlStateManager.Color());
+    }
+}
