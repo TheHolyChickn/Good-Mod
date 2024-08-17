39a619bfab7ec002e7cda4b945f3f31e8ad223ae
TheHolyChickn <theholychickn@gmail.com>
2024-08-17 02:02:52 -0400

    add "Yapping" class - wrapper for debug/logging  messages

---
+++ src/main/java/com/github/theholychickn/theholychicknaddons/owo/Yapping.java
@@ -0,0 +1,16 @@
+package com.github.theholychickn.theholychicknaddons.owo;
+
+import com.github.theholychickn.theholychicknaddons.GoodMod;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ChatComponentText;
+
+public class Yapping {
+
+    public static void log(String meow) {
+        GoodMod.Kitten.info("[good mod ]" + meow);
+    }
+
+    public static void chat(String meow) {
+        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("§r§3good mod §r§f» " + meow + "§r"));
+    }
+}
